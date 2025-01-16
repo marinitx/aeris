afbf5d4f9fe3ff768bfc16d9cedfe707b491e3e1
marinitx <mhiguera@student.42madrid.com>
2025-01-16 19:57:46 +0100

    a ver

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,4 @@ import { defineConfig } from 'astro/config';
 
 export default defineConfig({
   base: '/aeris/', // Cambia esto por el nombre de tu repositorio
-  build: {
-    assetsInlineLimit: 0,
-  },
 });
